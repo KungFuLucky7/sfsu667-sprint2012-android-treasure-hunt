04545318fb0841fe21b19a3e533ede28ced644a4
ibritten80@gmail.com <ibritten80@gmail.com@0c569c4b-f695-8b98-8594-2e47e4107bc1>
2012-04-08 06:00:28 +0000

    Initial Import of Treasure Hunt Project. Includes some stubs for Android backend.

---
+++ src/com/appspot/sfsutreasurehunt/SfsuTreasureHuntActivity.java
@@ -0,0 +1,13 @@
+package com.appspot.sfsutreasurehunt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SfsuTreasureHuntActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
