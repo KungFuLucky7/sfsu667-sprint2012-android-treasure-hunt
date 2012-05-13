c4b72e7504787cff18f086a84a57923ed7fa611c
ibritten80@gmail.com <ibritten80@gmail.com@0c569c4b-f695-8b98-8594-2e47e4107bc1>
2012-05-13 03:07:40 +0000

    Updated LoginActivity to connect to server, debugging is on

--- TreasureHuntReceiver/src/sfsu/treasurehunt/LoginActivity.java
+++ TreasureHuntReceiver/src/sfsu/treasurehunt/LoginActivity.java
@@ -1,7 +1,12 @@
 package sfsu.treasurehunt;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -127,23 +132,35 @@ public class LoginActivity extends Activity {
      */
     private void requestLogin() {
     	if(userNameBox.getText().toString().contentEquals("")) {
-    		Toast.makeText(getBaseContext(), "Enter a user name.", Toast.LENGTH_SHORT).show();
+    		Toast.makeText(getBaseContext(), "Enter a user name.", Toast.LENGTH_LONG).show();
     		Log.d("Login", "Blank user name field. ");
     		
     	} else if (passwordBox.getText().toString().contentEquals("")) {
-    		Toast.makeText(this, "Enter a password.", Toast.LENGTH_SHORT).show();
+    		Toast.makeText(this, "Enter a password.", Toast.LENGTH_LONG).show();
     		Log.d("Login", "Blank password field.");
     	} else {
-    		//networkCall(userNameBox.getText() + ", " + passwordBox.getText());
-    		isUserLoggedIn = true;
-    		action.setText("Start");
-    		Toast.makeText(this, "Network request sent.", Toast.LENGTH_SHORT).show();
-    		Log.d("Login", "Login to the server.");
     		userName = userNameBox.getText().toString();
     		password = passwordBox.getText().toString();
-    		closeLoginScreen();
+    		String[] loginInfo = {userName, password};
+    		new NetworkCall().execute(loginInfo);
     	}
     }
+    
+    
+    /*
+	 * Processes what happens after returning from a network call.
+	 */
+	protected void onNetworkResult() {
+		if(isUserLoggedIn) {
+			action.setText("Start");
+			Toast.makeText(this, "Log-In Succesful", Toast.LENGTH_SHORT).show();
+			Log.d("Login", "Login to the server.");
+			closeLoginScreen();
+		} else {
+			Toast.makeText(this, "Log-In Failed", Toast.LENGTH_SHORT).show();
+			Log.d("Login", "Not Logged in to the server.");
+		}
+	}
     
     /*
      * Bring up settings screen and associated buttons.
@@ -163,4 +180,69 @@ public class LoginActivity extends Activity {
         editor.commit();
         Log.d("Login", "Setting USERNAME = " + userName);
 	}
+	
+	/*
+	 * Makes a network request to the server.
+	 */
+    public class NetworkCall extends AsyncTask<String, Void, String> {
+        private final ProgressDialog dialog = new ProgressDialog(LoginActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            this.dialog.setMessage("Logging into game....");
+            Log.i("AsyncTask", "onPreExecute");
+        }
+
+        @Override
+        protected String doInBackground(String... sendingInfo) {
+
+        	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        	String URL = settings.getString("SERVER", "Error");
+
+            /*
+             * JSON for sending to server
+             * 
+             * String stringToJson = "{\"playerID\":\"" + sendingInfo[0] + "\", \"password\":\"" + sendingInfo[1]
+             * 	+ "\", \"currentLocation\":\"0,0\", \"option\":\"signIn\"}";
+             * 
+             */
+
+            // Debugging Hard-coded JSON
+            String stringToJson = "{\"playerID\":\"DF\", \"password\":\"testpass\", \"currentLocation\":\"121.235,-23.456\", \"option\":\"signIn\"}";
+            
+            JSONObject jsonToSend;
+            JSONObject responseJSON;
+            String signInStatus;
+            try {
+                jsonToSend = new JSONObject(stringToJson);
+            
+
+            HttpClient httpClient = new HttpClient();
+            responseJSON = httpClient.httpPost(URL, jsonToSend);
+
+            signInStatus = responseJSON.getString("signIn");
+            
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+            Log.i("NetworkCall", "doInBackgroup: " + URL);
+
+            return signInStatus;
+        }
+
+        /*
+         * On completion of Async task, the string pulled from the server is
+         * saved in passData.
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            if (this.dialog.isShowing()) {
+                this.dialog.dismiss();
+            }
+            Log.d("Networking", "result = " + result);
+            isUserLoggedIn = result.equalsIgnoreCase("Good");
+            onNetworkResult();
+        }
+    }
 }
